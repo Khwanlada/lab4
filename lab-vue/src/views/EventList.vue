448d0819362677d7d2300d84db1e02df94affe81
Khwanlada <khwanlada_k@cmu.ac.th>
2021-07-22 14:15:48 +0700

    2.6 Finish
    When click  Next page will go to a new page

--- lab-vue/src/views/EventList.vue
+++ lab-vue/src/views/EventList.vue
@@ -11,6 +11,7 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import {watchEffect} from '@vue/runtime-core'
 // import axios from 'axios'
 export default {
   name: 'EventList',
@@ -29,13 +30,16 @@ export default {
     }
   },
   created() {
-    EventService.getEvents(5, this.page)
+    watchEffect(() => {
+      EventService.getEvents(5, this.page)
       .then((response) => {
         this.events = response.data
       })
       .catch((error) => {
         console.log(error)
       })
+    })
+    
   }
 }
 </script>
